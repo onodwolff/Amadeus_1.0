b1788e3061a2e1ae248f2b29a6cfcc3477d5b3df
madwolff <bittaste@gmail.com>
2025-09-04 09:37:12 +0200

    18ю1

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,18 +1,21 @@
+// src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/app.routes';
+
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { demoApiInterceptor } from './app/interceptors/demo-api.interceptor';
+
 import { AppRootComponent } from './app/app-root.component';
 import { environment } from './environments/environment';
 
 const httpProviders = environment.demo
-  ? [provideHttpClient(withInterceptors([demoApiInterceptor]))]
-  : [provideHttpClient()];
+    ? [provideHttpClient(withInterceptors([demoApiInterceptor]))]
+    : [provideHttpClient()];
 
 bootstrapApplication(AppRootComponent, {
   providers: [
     provideRouter(routes),
     ...httpProviders,
   ],
-}).catch(err => console.error(err));
+}).catch((err: unknown) => console.error(err));
