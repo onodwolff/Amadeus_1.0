dca5929d5d93999e6a350035a6157054791d10b2
Volkov Zheka <87943906+onodwolff@users.noreply.github.com>
2025-09-05 20:48:49 +0200

    chore: prefix API endpoint

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   demo: false,
-  api: 'http://127.0.0.1:8100',
+  api: 'http://127.0.0.1:8100/api',
   ws:  'ws://127.0.0.1:8100/ws',
   token: ''
 };
