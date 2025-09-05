85537528cb815cd29990ef6d5efcc69e60b4af5b
Volkov Zheka <87943906+onodwolff@users.noreply.github.com>
2025-09-06 00:09:57 +0200

    chore: use PrimeUIX Aura theme

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -5,7 +5,7 @@ import { appRoutes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import { MessageService } from 'primeng/api';
-import Aura from '@primeng/themes/aura';
+import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
