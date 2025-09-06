835b6d17d8b86c612ef32df136a26a99e75bd547
Volkov Zheka <87943906+onodwolff@users.noreply.github.com>
2025-09-06 05:33:52 +0200

    Use environment-specific websocket base

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   demo: false,
   api: 'http://127.0.0.1:8100/api',
-  ws:  'ws://127.0.0.1:8100/ws',
+  ws:  'ws://127.0.0.1:8100/api/ws',
   token: ''
 };
